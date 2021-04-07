58570fac6f165141b755b4caa7c0e33b88cd945e
Jon Bakies <jon@baki.es>
2021-04-07 12:16:20 -0400

    try out software serial

--- Lora_Intro_RX/Lora_Intro_RX.ino
+++ Lora_Intro_RX/Lora_Intro_RX.ino
@@ -1,42 +1,56 @@
+#include <SoftwareSerial.h>
 /*This Code is for the RECEIVER*/
 
-String received;
+String received, response;
+SoftwareSerial loraSerial(D6, D5);
 
 void setup() {
   pinMode(BUILTIN_LED,OUTPUT);
   digitalWrite(BUILTIN_LED,LOW);
-  // put your setup code here, to run once:
-  Serial.begin(115200);   //default baudrate of module is 115200
-  delay(2000);             //wait for Lora device to be ready
 
-  Serial.print("AT\r\n");
-  delay(1000);
+  // Setup software serial interface on TX/RX pins 
+  loraSerial.begin(115200);
+
+  delay(2000);
+
+  loraSerial.print("AT\r\n");
+  // Module respondes with an +OK or something
+  Serial.print(loraSerial.readString());
   
   // https://reyax.com/wp-content/uploads/2020/01/Lora-AT-Command-RYLR40x_RYLR89x_EN.pdf
   // Page 4 for AT+PARAMETER
-  Serial.print("AT+PARAMETER=7,9,1,7\r\n");
-  delay(100);   //wait for module to respond
+  loraSerial.print("AT+PARAMETER=7,9,1,7\r\n");
+  Serial.print(loraSerial.readString());
   
-  Serial.print("AT+BAND=896000000\r\n");    //Bandwidth set to 868.5MHz
-  delay(100);   //wait for module to respond
+  loraSerial.print("AT+BAND=896000000\r\n");
+  Serial.print(loraSerial.readString());
   
-  Serial.print("AT+ADDRESS=170\r\n");   //needs to be unique
-  delay(100);   //wait for module to respond
+  loraSerial.print("AT+ADDRESS=170\r\n");
+  Serial.print(loraSerial.readString());
   
-  Serial.print("AT+NETWORKID=6\r\n");   //needs to be same for receiver and transmitter
-  delay(100);   //wait for module to respond
+  loraSerial.print("AT+NETWORKID=6\r\n");
+  Serial.print(loraSerial.readString());
 
   digitalWrite(BUILTIN_LED,HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available()){
-    received = Serial.readString();
-    if(received.startsWith("+RCV") && received[11]=='1'){  //in this case our single received byte would always be at the 11th position
+  if(loraSerial.available()){
+    received = loraSerial.readString();
+
+    // Receiving a "1" blinks the light.
+    if(received.startsWith("+RCV") && received[11]=='1'){
         digitalWrite(BUILTIN_LED,LOW);
         delay(100);
         digitalWrite(BUILTIN_LED,HIGH);
+
+    // Otherwise just print the message received to the other serial channel
+    } else { 
+        Serial.print(received);
     }
+
+  // Error
+  } else {
+    Serial.println("lora serial not available");
   }
 }
